d86d9a5d9700a7d1aa2088f9f2d27fc6c7f8a5e9
Zhyov <franchutornof@gmail.com>
2025-05-19 20:04:04 -0300

    Script

--- api.py
+++ api.py
@@ -8,23 +8,23 @@ CORS(app)
 
 def load_words():
     try:
-        with open('words.json', 'r', encoding='utf-8') as file:
+        with open("words.json", "r", encoding="utf-8") as file:
             return json.load(file)
     except FileNotFoundError:
         return []
 
-@app.route('/')
+@app.route("/")
 def home():
     return jsonify({"message": "Connected to Eshakap API"})
 
-@app.route('/get')
+@app.route("/get")
 def search_words():
-    query = request.args.get('q', '').lower()
+    query = request.args.get("q", "").lower()
 
     results = []
     if query:
         for word in load_words():
-            if query in word['word'].lower() or any(query in meaning.lower() for meaning in word['meaning']):
+            if query in word["word"].lower() or any(query in meaning.lower() for meaning in word["meaning"]):
                 word["id"] = str(uuid.uuid4())
                 results.append(word)
     else:
@@ -35,10 +35,51 @@ def search_words():
 
     return jsonify(results)
 
-@app.route('/max')
+@app.route("/max")
 def get_all_words_count():
     words = load_words()
     return jsonify({"max": len(words)})
 
-if __name__ == '__main__':
+@app.route("convert")
+def convert_to_script():
+    query = request.args.get("q", "").lower()
+    chararacters = query.split("")
+    charPath = "https://zhyov.github.io/Eshakap/assets/char/"
+    consonants = ["p", "b", "f", "v", "w", "k", "g", "t", "d", "đ", "z", "ž", "h", "j", "l", "m", "n", "ň", "r", "s", "š", "c", "č", "ç"]
+    vowels = ["a", "ä", "ą", "i", "į", "o", "ö"]
+    eshakap = []
+    final = []
+    count = 0
+
+    for index, char in enumerate(chararacters):
+        prev = chararacters[index - 1]
+        next = chararacters[index + 1]
+
+        if char in vowels and prev not in consonants:
+            final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}aläp.svg" })
+            final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{char}.svg" })
+            count += 2
+        elif char in consonants and (not next or next in consonants):
+            final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{char}.svg" })
+            final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}∅.svg" })
+            count += 2
+        elif char in consonants and next in vowels:
+            final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{char}.svg" })
+            final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{next}.svg" })
+            count += 2
+            index += 1
+        else:
+            final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{char}.svg" })
+            count += 2
+
+        if len(final) == 2:
+            eshakap.append({ "id": f"{str(uuid.uuid4())}", "syllable": final })
+            final = []
+    
+    if len(final) > 0:
+        eshakap.append({ "id": f"{str(uuid.uuid4())}", "syllable": final })
+
+    return jsonify(eshakap)
+
+if __name__ == "__main__":
     app.run()
