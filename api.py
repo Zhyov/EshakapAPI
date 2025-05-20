11c7d17ea1e64691e02a9054cef63aa8bf53a944
Zhyov <franchutornof@gmail.com>
2025-05-19 21:28:02 -0300

    Fix 5

--- api.py
+++ api.py
@@ -43,45 +43,38 @@ def get_all_words_count():
 @app.route("/convert")
 def convert_to_script():
     query = request.args.get("q", "").lower()
-    chararacters = list(query)
+    characters = list(query)
     charPath = "https://zhyov.github.io/Eshakap/assets/char/"
     consonants = ["p", "b", "f", "v", "w", "k", "g", "t", "d", "đ", "z", "ž", "h", "j", "l", "m", "n", "ň", "r", "s", "š", "c", "č", "ç"]
     vowels = ["a", "ä", "ą", "i", "į", "o", "ö"]
     eshakap = []
     final = []
     count = 0
-    skip = False
+    i = 0
 
-    for index, char in enumerate(chararacters):
-        if not skip:
-            prev = chararacters[index - 1]
-            try:
-                next = chararacters[index + 1]
-            except IndexError:
-                next = None
+    while i < len(characters):
+        char = characters[i]
+        prev = characters[i - 1] if i > 0 else None
+        next = characters[i + 1] if i + 1 < len(characters) else None
 
-            if char in vowels and prev not in consonants:
-                final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}aläp.svg" })
-                final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{char}.svg" })
-                count += 2
-            elif char in consonants and (not next or next in consonants):
-                final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{char}.svg" })
-                final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}∅.svg" })
-                count += 2
-            elif char in consonants and next in vowels:
-                final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{char}.svg" })
-                final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{next}.svg" })
-                count += 2
-                skip = True
-            else:
-                final.append({ "id": f"{str(uuid.uuid4())}", "path": f"{charPath}{char}.svg" })
-                count += 1
-
-            if len(final) == 2:
-                eshakap.append({ "id": f"{str(uuid.uuid4())}", "syllable": final })
-                final = []
+        if char in vowels and (prev not in consonants):
+            final.append({ "id": str(uuid.uuid4()), "path": f"{charPath}aläp.svg" })
+            final.append({ "id": str(uuid.uuid4()), "path": f"{charPath}{char}.svg" })
+        elif char in consonants and (next is None or next in consonants):
+            final.append({ "id": str(uuid.uuid4()), "path": f"{charPath}{char}.svg" })
+            final.append({ "id": str(uuid.uuid4()), "path": f"{charPath}∅.svg" })
+        elif char in consonants and next in vowels:
+            final.append({ "id": str(uuid.uuid4()), "path": f"{charPath}{char}.svg" })
+            final.append({ "id": str(uuid.uuid4()), "path": f"{charPath}{next}.svg" })
+            i += 1
         else:
-            skip = not skip
+            final.append({ "id": str(uuid.uuid4()), "path": f"{charPath}{char}.svg" })
+
+        if len(final) == 2:
+            eshakap.append({ "id": str(uuid.uuid4()), "syllable": final })
+            final = []
+
+        i += 1
     
     if len(final) > 0:
         eshakap.append({ "id": f"{str(uuid.uuid4())}", "syllable": final })
